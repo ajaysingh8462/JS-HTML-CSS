d0cdf02ef8430afb40e815098dcf776b8e8cadf2
ajaysingh8462 <ajaysingh84625@gmail.com>
2023-02-02 14:41:16 +0530

    [ajay]-all programs of html js and css

---
+++ add.js
@@ -0,0 +1,63 @@
+// // function Hello()
+// // {
+// //     console.log("Hello world");
+// // }
+// // Hello()
+
+// // function Hello2(firstnam,lastname)
+// // {
+// //     console.log("Hello "+firstnam+lastname);
+// // }
+// // Hello2("ajay","singh")
+
+// // //var is function scope
+// // // let is block scope
+
+// // var a=10;
+// // // var a is globle variable
+// // function data(){
+// //     var b=15;
+// //     // var b is local variable not acssesible out side of the function
+// //     console.log(a)
+// //     console.log(b)
+// // }
+// // data()
+// // let c=20;   
+// // function data1(){
+// //     let d=25;
+    
+// //     console.log(c)
+// //     console.log(d)
+// // }
+// // data1()
+// // function data3(){
+// //     let d=25;
+// //     if(d==25)
+    
+// //     console.log(c)
+// //     console.log(d)
+// // }
+// // data3()
+// // var a =10;
+// // var b=20;
+// // var c=a+b;
+// // console.log(c);
+
+// // Array
+// var arr =new Array("ajay","aditya","teena")
+// console.log(arr[arr.length-1]);
+
+// // short
+// console.log(arr.sort());
+// console.log(arr.reverse());
+// arr.push("Priya");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// // spread operator===== ... dot are importent only 3 dots 
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+// var data =[1,2,3];
+// sum(...data);
